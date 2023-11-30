42daba15c1b89ca6c652fa55ac7af20c347b8c1c
lamtruong <76791917+lamtrg279@users.noreply.github.com>
2023-11-30 15:56:13 -0600

    add componants

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Cards from "./Cards";
+import FormField from "./FormFIeld";
+import Loader from "./Loader";
+
+export { Cards, FormField, Loader }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import { Loader, Card, FormField } from '../components'
+
+const Home = () => {
+  const [loading, setLoading] = useState(false)
+  const [post, setPosts] = useState([])
+  
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
