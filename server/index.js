12c5277aaeab6623f4c207569d4a153f5fde6d1f
lamtruong <76791917+lamtrg279@users.noreply.github.com>
2023-12-15 11:55:16 -0600

    backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import * as dotenv from 'dotenv'
 import connectDB from './mongodb/connect.js'
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
 
 dotenv.config()
 
@@ -9,9 +11,13 @@ const app = express()
 app.use(cors())
 app.use(express.json({limit: '50mb'}))
 
+app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/dalle', dalleRoutes)
+
 app.get('/', async (req,res) => {
   res.send("Hello from DALL-E!")
 })
+
 
 const startServer = async() => {
   try {
